6b801b2b6f3c9737e94fd1b9eade9648b1d92754
Piko <afiqirfan158@gmail.com>
2026-07-31 15:20:39 +0800

    refactor: reorganize imports in ProjectsIndex component

--- resources/js/pages/projects/index.tsx
+++ resources/js/pages/projects/index.tsx
@@ -1,10 +1,11 @@
+import { Head, useForm } from '@inertiajs/react';
+import type { FormEvent } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
-import { Head, useForm } from '@inertiajs/react';
-import type { FormEvent } from 'react';
 
 type Project = {
     id: number;
