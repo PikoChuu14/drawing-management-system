ed7989ac1e76242a19b00a9ea7081f1663bacf23
Piko <afiqirfan158@gmail.com>
2026-07-31 14:47:00 +0800

    bug fixes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -42,13 +42,4 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-
-    /**
-     * Indicate that the model has two-factor authentication configured.
-     */
-    public function withTwoFactor(): static {
-
-        return $this;
-
-    }
 }
